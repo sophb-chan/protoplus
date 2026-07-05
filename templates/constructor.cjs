4e49b21a3270d84c1dcb254af8b39ccc38fbd5e1
Sophia Blair <sophb.ccjt@proton.me>
2026-07-05 06:28:28 +0000

    use prettier config

--- templates/constructor.cjs
+++ templates/constructor.cjs
@@ -1,27 +1,46 @@
-const fs = require("fs");
-const synchronizedPrettier = require("@prettier/sync");
-const path = require("path");
-const { cwd } = require("process");
+const fs = require('fs');
+const synchronizedPrettier = require('@prettier/sync');
+const path = require('path');
+const { cwd } = require('process');
 
 // get templates and filler
-const moduleTemplate = fs.readFileSync(path.join(__dirname, "protoplus.template.mjs"), "utf8");
-const webTemplate = fs.readFileSync(path.join(__dirname, "protoplus.template.js"), "utf8");
-const templateFiller = fs.readFileSync(path.join(__dirname, "protoplus.filler.js"), "utf8");
-
-// construct filled code and formatted code (using Prettier)
-const moduleFilled = webTemplate.replace(
-	"// protoplus goes here",
-	templateFiller,
+const moduleTemplate = fs.readFileSync(
+	path.join(__dirname, 'protoplus.template.mjs'),
+	'utf8'
 );
-const webFilled = webTemplate.replace("// protoplus goes here", templateFiller);
+const webTemplate = fs.readFileSync(
+	path.join(__dirname, 'protoplus.template.js'),
+	'utf8'
+);
+const templateFiller = fs.readFileSync(
+	path.join(__dirname, 'protoplus.filler.js'),
+	'utf8'
+);
+
+// construct filled code
+const moduleFilled = webTemplate.replace(
+	'// protoplus goes here',
+	templateFiller
+);
+const webFilled = webTemplate.replace('// protoplus goes here', templateFiller);
+// format code (using Prettier)
+const prettierRules = JSON.parse(
+	fs.readFileSync(path.join(__dirname, '../.prettierrc.json'), 'utf8')
+);
 const modulePretty = synchronizedPrettier.format(moduleFilled, {
-	parser: "babel",
+	parser: 'babel',
+	...prettierRules,
 });
-const webPretty = synchronizedPrettier.format(webFilled, { parser: "babel" });
-fs.writeFileSync(path.join(__dirname, "../protoplus.mjs"), modulePretty);
-fs.writeFileSync(path.join(__dirname, "../protoplus.js"), webPretty);
+const webPretty = synchronizedPrettier.format(webFilled, { parser: 'babel' });
+fs.writeFileSync(path.join(__dirname, '../protoplus.mjs'), modulePretty);
+fs.writeFileSync(path.join(__dirname, '../protoplus.js'), webPretty);
 
 // update version number in package.json
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json"), "utf8"));
+const packageJson = JSON.parse(
+	fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
+);
 packageJson.version = templateFiller.match(/(['"]?)version\1: "([^"]+)"/)[2];
-fs.writeFileSync(path.join(__dirname, "../package.json"), JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(
+	path.join(__dirname, '../package.json'),
+	JSON.stringify(packageJson, null, 2)
+);
